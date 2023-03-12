796418d58d660575ac7c64810e8f4148f7173ecd
Sebastian Kaier <22768625+skaiwlkr@users.noreply.github.com>
2023-03-12 14:46:30 +0100

    added scss and it's foundation styles

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,10 +1,19 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  css: ['~/assets/styles/tailwind.css'],
+  css: ['~/assets/styles/main.scss'],
   postcss: {
     plugins: {
       tailwindcss: {},
       autoprefixer: {},
     },
   },
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "@/assets/styles/foundation/foundation.scss" as *;'
+        }
+      }
+    }
+  }
 })
